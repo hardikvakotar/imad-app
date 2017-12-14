aaf4173b4bb0de82a4ed2d08bc0756a3ee187a25
hardikvakotar <vakotarhardikh@gmail.com>
2017-12-14 10:09:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.get('/article-one',function(req,res){
 app.get('/index',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-three',function(req,res){
-   res.send('article three is requested here');
-});
+//app.get('/article-three',function(req,res){
+  // res.send('article three is requested here');
+//});
 app.get('/article-two',function(req,res){
    res.send('article two is requested here');
 });
